eadd80e830f52cfdf3e94d82d9270346341a2ea1
isel42225 <42225@alunos.isel.ipl.pt>
2019-09-12 16:30:54 +0100

    Little Schemer, page 48 - 56

--- little-schemer/chapter-3.scm
+++ little-schemer/chapter-3.scm
@@ -12,3 +12,56 @@
 		(cond
 			((null? l) '())
 			(else (cons (car (car lat)) (firsts (cdr l)))))))
+
+(define insertR 
+  (lambda (new old lat)
+    (cond 
+      ((null? lat) '())
+      ((eq? old (car lat)) (cons old (cons new (cdr lat))))
+      (else 
+        (cons (car lat) (insertR new old (cdr lat)))))))
+
+(define insertL
+  (lambda (new old lat)
+    (cond 
+      ((null? lat) '())
+      ((eq? old (car lat)) (cons new lat))
+      (else 
+        (cons (car lat) (insertL new old (cdr lat)))))))
+
+(define subst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? old (car lat)) (cons new (cdr lat)))
+      (else 
+        (cons (car lat) (subst new old (cdr lat)))))))
+
+(define subst2 
+  (lambda (new o1 o2 lat)
+    (cond 
+      ((null? lat) '())
+      ((eq? o1 (car lat)) (cons new (cdr lat)))
+      ((eq? o2 (car lat)) (cons new (cdr lat)))
+      (else 
+        (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))
+
+(define multirember
+  (lambda (a lat)
+    (cond
+      ((null? lat) '())
+      ((eq? a (car lat)) (multirember a (cdr lat)))
+      (else 
+        (cons (car lat) (multirember a (cdr lat)))))))
+
+  (define multiinsertR
+    (lambda (new old lat)
+      (cond 
+        ((null? lat) '())
+        (else
+          (cond 
+            ((eq? old (car lat)) 
+              (cons old 
+                (cons new (multiinsertR new old (cdr lat)))))
+            (else 
+              (cons (car lat) (multiinsertR new old (cdr lat))))))))
